38493c3f0c3d4707a38fedabc7fd04d28d743a66
Chinh La <chechaohp@gmail.com>
2022-12-06 14:42:01 +0700

    update map

--- js/scripts.js
+++ js/scripts.js
@@ -295,7 +295,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-  var location = { lat: 22.5932759, lng: 88.27027720000001 };
+  var location = { lat: 20.8461707, lng: 106.7051749 };
   var map = new google.maps.Map(document.getElementById("map-canvas"), {
     zoom: 15,
     center: location,
@@ -309,7 +309,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-  var la_fiesta = { lat: 20.305826, lng: 85.85480189999998 };
+  var la_fiesta = { lat: 20.8461707, lng: 106.7051749 };
   var map = new google.maps.Map(document.getElementById("map-canvas"), {
     zoom: 15,
     center: la_fiesta,
